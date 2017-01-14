aaa3185a0b5c2f52a7732882b0ac8c3a1561cfad
Patrick Smith <patrick@promptworks.com>
2017-01-14 16:04:34 -0500

    Add ObjC action extension instead of Swift

---
+++ ios/ActionExt/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  ActionExt
+//
+//  Created by Patrick Smith on 1/14/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
